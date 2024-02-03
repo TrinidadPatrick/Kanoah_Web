b4bc65f9e467c5b85e812fa606ebe1573f8b19ba
John Patrick Trinidad <Ptrinidad765@gmail.com>
2024-02-03 13:34:22 +0800

    updated view booking detail modal

--- src/Pages/AccountSetting/UserBookings/UserCompletedBookings.js
+++ src/Pages/AccountSetting/UserBookings/UserCompletedBookings.js
@@ -17,13 +17,15 @@ const UserCompletedBookings = ({completedBookings}) => {
           bottom: 'auto',
           marginRight: '-50%',
           transform: 'translate(-50%, -50%)',
-          padding : "0"
+          padding: '0',
+          width: 'fit-content',  // Set the width to fit-content
+          maxWidth: '90%',       // Optional: You can set a maximum width if needed
         },
         overlay: {
-            backgroundColor: 'rgba(0, 0, 0, 0.5)', // Change the color and opacity as needed
-            zIndex: 998,
-          },
-    };
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          zIndex: 998,
+        },
+      };
 
     const openBookingInfo = (id) => {
         const selected = completedBookings.find(booking => booking._id === id)
@@ -125,76 +127,76 @@ const UserCompletedBookings = ({completedBookings}) => {
 
 
     <Modal  isOpen={modalIsOpen} style={ModalStyle}>
-        <div className='w-fit h-fit flex flex-col bg-[#f9f9f9] border p-2'>
+        <div className='w-full sm:w-fit h-[90dvh] sm:h-full flex flex-col bg-[#f9f9f9] p-2'>
             <div className='flex items-center space-x-1'>
-            <ArrowBackIosNewOutlinedIcon onClick={()=>setIsOpen(false)} fontSize='small' className='text-gray-500 cursor-pointer' />
-            <h1 className='text-xl text-gray-800 font-medium'>Booking Details</h1>
+            <ArrowBackIosNewOutlinedIcon onClick={()=>setIsOpen(false)} fontSize='small' className='text-gray-500 p-0.5 cursor-pointer' />
+            <h1 className='text-lg text-gray-700 font-medium'>Booking Details</h1>
             </div>
             
-            <div  className="flex flex-col h-fit my-4  p-2">
+            <div  className="flex flex-col h-fit my-0 sm:my-4  p-2">
                 {/* Image and personal info container */}
                 <div className='w-full flex flex-col sm:flex-row bg-white rounded-md border shadow-sm p-2'>
                     {/* Image Container */}
-                    <div className='h-[180px] aspect-square flex '>
+                    <div className='h-[120px] md:h-[140px] aspect-square flex '>
                         <img className='w-full rounded-md h-full object-cover' src={clientInformation?.shop.serviceProfileImage} alt="image" />
                     </div>
                     {/* Booking Information */}
-                    <div className='flex flex-col space-y-2 mt-3 sm:mt-0 px-2'>
-                    <div className='text-xl font-semibold text-gray-900 '>{clientInformation?.service?.selectedService}</div>
+                    <div className='flex flex-col space-y-0 sm:space-y-2 px-2'>
+                    <div className='text-base md:text-lg font-semibold text-themeOrange '>{clientInformation?.service?.selectedService}</div>
                     <div className='w-fit h-full grid grid-cols-2 gap-0 gap-x-5 '>
                         {/* Variant */}
-                        <div className={`text-semiMd font-semibold whitespace-nowrap  `}>Variant</div>
-                        <div className={`font-medium text-semiMd text-gray-600 `}>{clientInformation?.service.selectedVariant ? clientInformation?.service.selectedVariant?.type : "None"}</div>
+                        <div className={`text-[0.72rem] md:text-sm font-medium text-gray-800 whitespace-nowrap  `}>Variant</div>
+                        <div className={`font-normal text-semiMd text-gray-600 `}>{clientInformation?.service.selectedVariant ? clientInformation?.service.selectedVariant?.type : "None"}</div>
                         {/* Amount */}
-                        <div className='text-semiMd font-semibold whitespace-nowrap'>Amount</div>
-                        <div className='font-medium text-semiMd text-gray-600'>₱ {clientInformation?.service.price}</div>
+                        <div className='text-xs md:text-[0.8rem] font-medium text-gray-800 whitespace-nowrap'>Amount</div>
+                        <div className='font-normal text-xs md:text-[0.8rem] text-gray-600'>₱ {clientInformation?.service.price}</div>
                         {/* Schedule */}
-                        <div className='text-semiMd font-semibold whitespace-nowrap'>Schedule</div>
-                        <div className='font-medium text-semiMd text-gray-600'>{clientInformation?.bookSchedule}</div>
+                        <div className='text-xs md:text-[0.8rem] font-medium text-gray-800 whitespace-nowrap'>Schedule</div>
+                        <div className='font-normal text-xs md:text-[0.8rem] text-gray-600'>{clientInformation?.bookSchedule}</div>
                         {/* Time */}
-                        <div className='text-semiMd font-semibold whitespace-nowrap'>Time</div>
-                        <div className='font-medium text-semiMd text-gray-600'>{clientInformation?.schedule.bookingTime}</div>
+                        <div className='text-xs md:text-[0.8rem] font-medium text-gray-800 whitespace-nowrap'>Time</div>
+                        <div className='font-normal text-xs md:text-[0.8rem] text-gray-600'>{clientInformation?.schedule.bookingTime}</div>
                         {/* Time */}
-                        <div className='text-semiMd font-semibold whitespace-nowrap'>Service Option</div>
-                        <div className='font-medium text-semiMd text-gray-600'>{clientInformation?.schedule.serviceOption}</div>
+                        <div className='text-xs md:text-[0.8rem] font-medium text-gray-800 whitespace-nowrap'>Service Option</div>
+                        <div className='font-normal text-xs md:text-[0.8rem] text-gray-600'>{clientInformation?.schedule.serviceOption}</div>
                     </div>
                     </div>
                     
                 </div>
                 <div className='w-full mt-4 flex flex-col bg-white rounded-md border shadow-sm p-2'>
-                    <h2 className='text-gray-800 font-medium'>Client Details</h2>
-                    <span className='text-gray-600 font-normal text-sm'>{clientInformation?.contactAndAddress.firstname + " " + clientInformation?.contactAndAddress.lastname}</span>
-                    <span className='text-gray-600 font-normal text-sm'>{clientInformation?.contactAndAddress.email}</span>
-                    <span className='text-gray-600 font-normal text-sm'>+63{clientInformation?.contactAndAddress.contact}</span>
-                    <span className='text-gray-600 font-normal text-sm'>{clientInformation?.contactAndAddress.Address.barangay.name + ", " + clientInformation?.contactAndAddress.Address.province.name + ", " + clientInformation?.contactAndAddress.Address.region.name}</span>
-                    <span className='text-gray-600 font-normal text-sm'>{clientInformation?.contactAndAddress.Address.street}</span>
+                    <h2 className='text-gray-800 text-sm font-medium'>Client Details</h2>
+                    <span className='text-gray-600 font-normal text-[0.7rem]'>{clientInformation?.contactAndAddress.firstname + " " + clientInformation?.contactAndAddress.lastname}</span>
+                    <span className='text-gray-600 font-normal text-[0.7rem]'>{clientInformation?.contactAndAddress.email}</span>
+                    <span className='text-gray-600 font-normal text-[0.7rem]'>+63{clientInformation?.contactAndAddress.contact}</span>
+                    <span className='text-gray-600 font-normal text-[0.7rem]'>{clientInformation?.contactAndAddress.Address.barangay.name + ", " + clientInformation?.contactAndAddress.Address.province.name + ", " + clientInformation?.contactAndAddress.Address.region.name}</span>
+                    <span className='text-gray-600 font-normal text-[0.7rem]'>{clientInformation?.contactAndAddress.Address.street}</span>
                 </div>
                 <div className='w-full mt-4 grid grid-cols-2 gap-0 gap-x-5 bg-white rounded-md border shadow-sm p-2'>
-                    <div className='text-semiMd font-semibold whitespace-nowrap'>BOOKING ID</div>
-                    <div className='font-medium text-right text-semiMd text-gray-600'>{clientInformation?.Booking_id}</div>
+                    <div className='text-xs md:text-xs font-semibold whitespace-nowrap'>BOOKING ID</div>
+                    <div className='font-medium text-right text-xs md:text-xs text-gray-600'>{clientInformation?.booking_id}</div>
 
-                    <div className='text-semiMd font-semibold whitespace-nowrap'>Book Date</div>
-                    <div className='font-medium text-right text-semiMd text-gray-600'>{clientInformation?.issuedDate}</div>
+                    <div className='text-xs md:text-xs font-semibold whitespace-nowrap'>Book Date</div>
+                    <div className='font-medium text-right text-xs md:text-xs text-gray-600'>{clientInformation?.issuedDate}</div>
 
-                    <div className='text-semiMd font-semibold whitespace-nowrap'>Book Schedule</div>
-                    <div className='font-medium text-right text-semiMd text-gray-600'>{clientInformation?.schedule.timeSpan[0]} - {clientInformation?.schedule.timeSpan[1]}</div>
+                    <div className='text-xs md:text-xs font-semibold whitespace-nowrap'>Book Schedule</div>
+                    <div className='font-medium text-right text-xs md:text-xs text-gray-600'>{clientInformation?.schedule.timeSpan[0]} - {clientInformation?.schedule.timeSpan[1]}</div>
                     
                 </div>
                 <div className='w-full mt-4 grid grid-cols-2 gap-0 gap-x-5 bg-white rounded-md border shadow-sm p-2'>
-                    <div className='text-semiMd font-semibold whitespace-nowrap'>Service Amount</div>
-                    <div className='font-medium text-right text-semiMd text-gray-600'>₱{clientInformation?.service.price}</div>
+                    <div className='text-xs md:text-xs font-semibold whitespace-nowrap'>Service Amount</div>
+                    <div className='font-medium text-right text-xs md:text-xs text-gray-600'>₱{clientInformation?.service.price}</div>
 
-                    <div className='text-semiMd font-semibold whitespace-nowrap'>Booking Fee</div>
-                    <div className='font-medium text-right text-semiMd text-gray-600'>₱{clientInformation?.booking_fee}</div>
+                    <div className='text-xs md:text-xs font-semibold whitespace-nowrap'>Booking Fee</div>
+                    <div className='font-medium text-right text-xs md:text-xs text-gray-600'>₱{clientInformation?.booking_fee}</div>
 
-                    <div className='text-semiMd font-semibold whitespace-nowrap'>Service Fee</div>
-                    <div className='font-medium text-right text-semiMd text-gray-600'>₱{clientInformation?.service_fee}</div>
+                    <div className='text-xs md:text-xs font-semibold whitespace-nowrap'>Service Fee</div>
+                    <div className='font-medium text-right text-xs md:text-xs text-gray-600'>₱{clientInformation?.service_fee}</div>
 
-                    <div className='text-semiMd font-semibold whitespace-nowrap'>Total Amount</div>
-                    <div className='font-medium text-right text-semiMd text-red-500'>₱{clientInformation?.net_Amount}</div>
+                    <div className='text-xs md:text-xs font-semibold whitespace-nowrap'>Total Amount</div>
+                    <div className='font-medium text-right text-xs md:text-xs text-red-500'>₱{clientInformation?.net_Amount}</div>
 
-                    <button className='bg-gray-100 border rounded-sm text-sm text-gray-800 py-1 mt-2'>View Service</button>
-                    <button className='bg-green-400 border rounded-sm text-sm text-gray-100 py-1 mt-2'>Contact Service</button>
+                    <button className='bg-gray-100 border rounded-sm text-xs text-gray-800 py-1 mt-2'>View Service</button>
+                    <button className='bg-green-400 border rounded-sm text-xs text-gray-100 py-1 mt-2'>Contact Service</button>
                     </div>
                 </div>
         </div>
