4d967974db2af168962863ba82e851258dcdd539
John Patrick Trinidad <Ptrinidad765@gmail.com>
2024-07-05 21:02:19 +0800

    updated

--- src/http.js
+++ src/http.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL : "http://localhost:5000/api/",
-    // baseURL : "https://kanoah.onrender.com/api/",
+    // baseURL : "http://localhost:5000/api/",
+    baseURL : "https://kanoah.onrender.com/api/",
     headers : {
         "Content-Type" : "Application/json"
     }
